f827d64a1e64f27f0cd6b4ddf6c252c081ecc84d
MitosAI <vijayt@gensigma.com>
2026-09-06 19:40:59 -0700

    GEN-9: add benchmark package

---
+++ src/gensigma_benchmarks/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic architecture benchmark contracts for GenSigma."""
+
+from .business_reality import BenchmarkRunner, ReferenceAdapter, build_fixture
+
+__all__ = ["BenchmarkRunner", "ReferenceAdapter", "build_fixture"]
