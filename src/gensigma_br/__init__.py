38cd6630d9d5931713a3eb0008d4e540073cca84
MitosAI <vijayt@gensigma.com>
2026-09-06 23:09:34 -0700

    merge: preserve temporal exports with relationship errors

--- src/gensigma_br/__init__.py
+++ src/gensigma_br/__init__.py
@@ -2,16 +2,21 @@
 
 from .contracts import ContractRegistry, ContractViolation, UnknownSemanticType
 from .kernel import (
+    AmbiguousTemporalState,
     BusinessRealityKernel,
     CandidateSemanticTypeMismatch,
     CanonicalResourceConflict,
     EvidenceConflict,
     RelationshipInvariantViolation,
+    TemporalQueryError,
+    TemporalStateNotFound,
     UnknownCandidate,
     UnknownEvidence,
+    UnsupportedTemporalPrecision,
 )
 
 __all__ = [
+    "AmbiguousTemporalState",
     "BusinessRealityKernel",
     "CandidateSemanticTypeMismatch",
     "CanonicalResourceConflict",
@@ -19,7 +24,10 @@ __all__ = [
     "ContractViolation",
     "EvidenceConflict",
     "RelationshipInvariantViolation",
+    "TemporalQueryError",
+    "TemporalStateNotFound",
     "UnknownCandidate",
     "UnknownEvidence",
     "UnknownSemanticType",
+    "UnsupportedTemporalPrecision",
 ]
